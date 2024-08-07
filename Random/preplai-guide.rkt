313b981377cf28aee786525215c1f2591e26de79
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-08-07 13:30:29 -0400

    some more of Tanter's exercises

--- Random/preplai-guide.rkt
+++ Random/preplai-guide.rkt
@@ -1,26 +1,32 @@
 #lang slideshow
 
+; declaring to variables locally and returning the maximum one
 (let ([x 4]
       [y 5])
   (max x y))
 
+; sums a and b
 (define (sum a b)
   (+ a b))
 
-; elige elemento aleatorio entre x e y
+; chooses random element between x and y
 (define (pick-random x y)
   (define rand (random))
   (if (< rand 1/2)
       x y))
 
-; suma 1 a cada elemento del par p
+; sums 1 to each element on pair p
 (define (pair-add1 p)
   (define x (+ (car p) 1))
   (define y (+ (cdr p) 1))
   (cons x y))
 
-; retorna elemento aleatorio de l
+; returns random element from l
 (define (list-pick-random l)
-  (define k (length l))
-  (define rand (random k))
+  (define rand (random (length l)))
   (list-ref l rand))
+
+; switches random value on v with given x
+(define (vector-set-random! v x)
+  (define rand (random (vector-length v)))
+  (vector-set! v rand x))
