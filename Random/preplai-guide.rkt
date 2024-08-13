66ee60f06cab6d15b334e75af2ddb5f8ac3d9e98
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-08-13 17:04:38 -0400

    progressed on preplay guide

--- Random/preplai-guide.rkt
+++ Random/preplai-guide.rkt
@@ -106,6 +106,7 @@
   (lambda (x) (not (p x))))
 
 ; 2.
+; defined before
 
 ; 3.
 (define (apply-twice f)
@@ -113,5 +114,100 @@
 
 (define (apply-twice2 f x)
   (f (f x)))
+
 (define (apply-twice-2 f x y)
   (f (f x y) (f x y )))
+
+; findf return first element that is a multiple of 13 or if there's not, returns #f
+(define (findf lst)
+  (ormap (lambda (x) (and (zero? (modulo x 13)) x)) lst))
+
+(test (findf '(1 2 3)) #f)
+(test (findf '(1 13 3)) 13)
+(test (findf '(1 3 26 3 39)) 26)
+
+; reject with negate and filter
+(define (reject2 lst pred)
+  (filter (negate pred) lst))
+
+(test (reject2 lst pred1) '(1 3 5))
+(test (reject2 lst pred2) '(2 4 6))
+
+; curry given a function of type (AB -> C) returns an equivalent of type A->(B->C)
+(define (curry f)
+  (lambda (a)
+    (lambda (b)
+      (f a b))))
+
+; memoize given a function of type (A -> B) returns an equivalent function that holds a cache <argument->value> on a hash table
+(define (memoize f)
+  (define cache (make-hash))
+  (lambda (x)
+    (let ([cached (hash-ref cache x #f)])
+      (if cached
+          cached
+          (let ([result (f x)])
+            (hash-set! cache x result)
+            result)))))
+
+; tests with square
+(define (square x) (* x x))
+(define memoized-square (memoize square))
+
+(test (memoized-square 5) 25)
+(test (memoized-square 5) 25)
+(test (memoized-square 7) 49)
+
+; length :: List -> Number
+; retorna el largo de la lista
+(define (length l)
+  (match l
+    [(list) 0]
+    [(cons h t) (+ 1 (length t))]))
+ 
+(test (length (list)) 0)
+(test (length '(1 2 3 4)) 4)
+
+; sum-list :: List Number -> Number
+; sums the elements of a list
+(define (sum-list lst)
+  (match lst
+    [(list) 0]
+    [(cons h t) (+ h (sum-list t))]))
+
+(test (sum-list '(2 0 7)) 9)
+(test (sum-list '(1 2 3)) 6)
+(test (sum-list '()) 0)
+
+; my-reverse :: List Any -> List Any
+; returns the same list reversed
+(define (my-reverse lst)
+  (define (reverse-helper lst acc)
+    (match lst
+      [(list) acc]
+      [(cons h t) (reverse-helper t (cons h acc))]))
+  (reverse-helper lst '())) 
+
+(test (my-reverse '()) '())
+(test (my-reverse '(1 2 3 4 5)) '(5 4 3 2 1))
+(test (my-reverse '(1 6 2 43 5 90)) '(90 5 43 2 6 1))
+
+; my-map :: Proc ListAny -> List Any
+; map proc on lst and return the mapped list
+(define (my-map proc lst)
+  (match lst
+   [(list) '()]
+   [(cons h t) (cons (proc h) (map proc t))]))
+
+(test (my-map (lambda (number) (+ 1 number)) '(1 2 3 4)) '(2 3 4 5))
+(test (my-map even? '(1 2 3 4)) '(#f #t #f #t))
+
+; my-foldl :: Proc Init ListAny -> Any
+
+(define (my-foldl f init lst)
+  (match lst
+    [(list) init]
+    [(cons h t) (my-foldl f (f init h) t)]))
+
+(test (my-foldl cons '() '(1 2 3 4)) '((((() . 1) . 2) . 3) . 4))
+(test (my-foldl + 0 '(1 2 3 4)) 10)
