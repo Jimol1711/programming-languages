e483b321fa3dbfe2e0a2e8e41e8d95cc41ab5666
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-08-27 17:10:56 -0400

    only imp of rac-to-cf left

--- Tareas/T1/T1.rkt
+++ Tareas/T1/T1.rkt
@@ -1,9 +1,11 @@
 #lang play
 (require math/flonum)
 
+;; Juan Ignacio Molina - Sección 1
+
 #|
-Hizo Ud uso de la whiteboard policy:  (Indique SI/NO)
-En caso que afirmativo, indique con quién y sobre qué ejercicio:
+Hizo Ud uso de la whiteboard policy:  NO
+En caso que afirmativo, indique con quién y sobre qué ejercicio: 
 |#
 
 ;; Parte a)
@@ -12,7 +14,7 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
 <CFraction> ::= (simple <value>)
              |  (compound <value> <value> <CFraction>)
 |#
-; Datatype to represent a finite continuous fraction to integer coefficients
+;; Datatype to represent a finite continuous fraction to integer coefficients
 (deftype CFraction
   (simple value)
   (compound first-value second-value c-fraction))
@@ -29,6 +31,7 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
                           (+ a (/ b (eval d))))]))
 
 ;; Parte c)
+
 ;; degree ::  CFraction -> Integer
 ;; Returns the degree of c-fraction
 (define (degree c-fraction)
@@ -39,6 +42,7 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
                           (+ 1 (degree d)))]))
 
 ;; Parte d)
+
 ;; fold-cfraction :: (Integer -> A) (Integer Integer A -> A) -> (CFraction -> A)
 ;; Captures the recursive schema of CFraction
 (define (fold-cfraction simp rec)
@@ -48,6 +52,7 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
       [(compound a b d) (rec a b ((fold-cfraction simp rec) d))])))
 
 ;; Parte e)
+
 ;; eval2 :: CFraction -> Rational
 ;; Returns the rational value resulting of evaluating a given c-fraction
 (define eval2
@@ -65,42 +70,38 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
    (λ (a b d) (+ 1 d))))
 
 ;; Parte f)
+
 ;; mysterious-cf :: Integer -> CFraction
 ;; Generates a mysterious continous fractions
 (define (mysterious-cf value)
-  (if (> 0 value)
+  (if (< value 0)
       (error "Error: argumento negativo")
-      (match value
-        [0 (simple 6)]
-        [v (compound 6 (sqr (- (* 2 v) 1))(mysterious-cf (- v 1)))])))
-
-;; (define (mysterious-cf value)
-;;   (cond [(< value 0) (error "Error: argumento negativo")]
-;;         [(zero? value) (simple 6)]
-;;         [else (compound 6 (sqr (- (* 2 (- value 1)) 1))(mysterious-cf (- value 1)))]))
-
-;; (define (mysterious-cf value)
-;;   (define (helper current-value)
-;;     (if (= current-value value)
-;;         (simple 6)
-;;         (compound 6 (sqr (+ (* 2 current-value) 1)) (helper (+ current-value 1)))))
-;;   (helper 0))
-
-(simple 6)
-(compound 6 (sqr 1) (simple 6))
-(compound 6 (sqr 1) (compound 6 (sqr 3)(simple 6)))
-(compound 6 (sqr 1) (compound 6 (sqr 3) (compound 6 (sqr 5)(simple 6))))
-
+      (foldl
+       (lambda (v acc) (compound 6 (sqr (- (* 2 v) 1)) acc))
+       (simple 6)
+       (range value 0 -1)))) ; Esto tuve que hacerlo así porque la recursión me generaba que la fracción se retornaba invertida.
 
 ;; Parte g)
-;; from-to :: Integer -> Integer -> ListOf Integer
 
+;; from-to :: Integer Integer -> ListOf Integer
+;; Constructs a list of integers between two given integers
+(define (from-to i j)
+  (if (> i j)
+      (from-to j i)
+      (range i j)))
 
-;; mysterious-list :: Integer -> listOf Float
-
+;; mysterious-list :: Integer -> ListOf Float
+;; Returns a list such that the ith element is the difference between the evaluation of (mysterious-cf i) and 3
+(define (mysterious-list n)
+      (map (λ (x) (- (fl (eval (mysterious-cf x))) 3)) (from-to 0 (+ n 1))))
 
 ;; A que numero tiende (mysterious-cf k) cuando k tiende a infinito?
 
+;; Dandole a mysterious-list un n relativamente alto podemos ver que (mysterious-cf k) tiende a pi cuando k tiende a infinito
+;; Esto se prueba con varios valores de n en los tests
 
 ;; Parte h)
 ;; rac-to-cf :: Rational -> CFraction
+;; Transforms a non-negative rational number into it's CFraction representation
+(define (rac-to-cf r)
+  (void))
--- Tareas/T1/test.rkt
+++ Tareas/T1/test.rkt
@@ -46,3 +46,24 @@
 (test (mysterious-cf 2) (compound 6 (sqr 1) (compound 6 (sqr 3)(simple 6))))
 (test (mysterious-cf 5) (compound 6 (sqr 1) (compound 6 (sqr 3)(compound 6 (sqr 5)(compound 6 (sqr 7)(compound 6 (sqr 9)(simple 6)))))))
 (test (mysterious-cf 8) (compound 6 (sqr 1) (compound 6 (sqr 3)(compound 6 (sqr 5)(compound 6 (sqr 7)(compound 6 (sqr 9)(compound 6 (sqr 11) (compound 6 (sqr 13)(compound 6 (sqr 15)(simple 6))))))))))
+
+;; tests for from-to
+(test (from-to 0 3) '(0 1 2))
+(test (from-to 3 0) '(0 1 2))
+(test (from-to 3 7) '(3 4 5 6))
+(test (from-to -7 3) '(-7 -6 -5 -4 -3 -2 -1 0 1 2))
+(test (from-to 3 -1) '(-1 0 1 2))
+
+;; tests for mysterious-list
+(test (mysterious-list 0) '(3.0))
+(test (mysterious-list 1)'(3.0 3.166666666666667))
+(test (mysterious-list 5) '(3.0 3.166666666666667 3.1333333333333337 3.1452380952380956 3.13968253968254 3.142712842712843))
+(test (mysterious-list 15) '(3.0 3.166666666666667 3.1333333333333337 3.1452380952380956 3.13968253968254 3.142712842712843 3.140881340881341 3.142071817071817 3.141254823607765 3.141839618929402 3.141406718496502 3.1417360992606653 3.141479689004255 3.141683189207755 3.141518985595275 3.1416533941974265))
+(test (mysterious-list 20) '(3.0 3.166666666666667 3.1333333333333337 3.1452380952380956 3.13968253968254 3.142712842712843 3.140881340881341 3.142071817071817 3.141254823607765 3.141839618929402 3.141406718496502 3.1417360992606653 3.141479689004255 3.141683189207755 3.141518985595275 3.1416533941974265 3.1415419859977822 3.141635356679388 3.1415563302845726 3.1416238066678384 3.1415657346585473))
+
+;; tests for rac-to-cf
+(test (rac-to-cf (+ 3 49/200)) (compound 3 1 (compound 4 1 (compound 12 1 (simple 4)))))
+(test (rac-to-cf 5.333333333333333) (compound 5 1 (simple 3)))
+(test (rac-to-cf 0) (simple 0))
+(test (rac-to-cf 4.0) (simple 4))
+(test (rac-to-cf 59/13) (compound 4 7 (simple 13)))
