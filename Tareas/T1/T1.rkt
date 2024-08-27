05e708611372eb031b961960d86d678a2fa65afd
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-08-26 22:31:23 -0400

    can't progress T1

--- Tareas/T1/T1.rkt
+++ Tareas/T1/T1.rkt
@@ -40,7 +40,7 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
 
 ;; Parte d)
 ;; fold-cfraction :: (Integer -> A) (Integer Integer A -> A) -> (CFraction -> A)
-;; Captures the recursive scheme of CFraction
+;; Captures the recursive schema of CFraction
 (define (fold-cfraction simp rec)
   (λ (c-fraction)
     (match c-fraction
@@ -62,12 +62,34 @@ En caso que afirmativo, indique con quién y sobre qué ejercicio:
 (define degree2
   (fold-cfraction
    (λ (value) 0)
-   (λ (a b d) (if (= d 0)
-                  (error "zero division")
-                  (+ 1 d)))))
+   (λ (a b d) (+ 1 d))))
 
 ;; Parte f)
 ;; mysterious-cf :: Integer -> CFraction
+;; Generates a mysterious continous fractions
+;; (define (mysterious-cf value)
+;;   (if (> 0 value)
+;;       (error "Error: argumento negativo")
+;;       (match value
+;;         [0 (simple 6)]
+;;         [value (compound 6 (sqr (value - 2))(mysterious-cf (value - 2)))])))
+
+;; (define (mysterious-cf value)
+;;   (cond [(< value 0) (error "Error: argumento negativo")]
+;;         [(zero? value) (simple 6)]
+;;         [else (compound 6 (sqr (- (* 2 (- value 1)) 1))(mysterious-cf (- value 1)))]))
+
+(define (mysterious-cf value)
+  (define (helper current-value)
+    (if (= current-value value)
+        (simple 6)
+        (compound 6 (sqr (+ (* 2 current-value) 1)) (helper (+ current-value 1)))))
+  (helper 0))
+
+(simple 6)
+(compound 6 (sqr 1) (simple 6))
+(compound 6 (sqr 1) (compound 6 (sqr 3)(simple 6)))
+(compound 6 (sqr 1) (compound 6 (sqr 3) (compound 6 (sqr 5)(simple 6))))
 
 
 ;; Parte g)
