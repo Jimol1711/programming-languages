31627c87bfb8e56155f08bd527bcc1be18644e6a
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-11-05 12:40:50 -0300

    aux8 ready

--- Auxiliares/aux8.rkt
+++ Auxiliares/aux8.rkt
@@ -1,5 +1,7 @@
 #lang play
 
+(print-only-errors #t)
+
 ; (require "base_aux8.rkt")
 
 ; P1.
@@ -19,12 +21,38 @@
     [0 '()]
     [_ (if (box? (car l))
            (append (list (car (unbox (car l)))) (take-list-n (cdr (unbox (car l))) (- n 1)))
-           (append (list (car l)) (take-list-n (cdr l))))])) ; TERMINAR
+           (append (list (car l)) (take-list-n (cdr l) (- n 1))))])) ; TERMINAR
 
-(test (take-list-n (cyclic-list (list 1 2 3) 6)) (list 1 2 3 1 2 3))
+(test (take-list-n (cyclic-list (list 1 2 3)) 5) (list 1 2 3 1 2))
+(test (take-list-n (cyclic-list (list 1 2 3)) 7) (list 1 2 3 1 2 3 1))
 
 ; c)
 ;; cyclic-list-to-mcons :: ListOf[A] -> Mcons(A ...)
+(define (cyclic-list-to-mcons lst)
+  (if (null? lst)
+      '() ; Return an empty list if the input is empty
+      (let* ([head (mcons (car lst) '())] ; Create the head of the list
+             [current head]) ; Start building from the head
+        ;; Loop through the list, creating mcons pairs
+        (for-each (lambda (x)
+                    (let ([new-node (mcons x '())])
+                      (set-mcdr! current new-node)
+                      (set! current new-node)))
+                  (cdr lst))
+        ;; Create the cycle by pointing the last node's cdr to the head
+        (set-mcdr! current head)
+        head)))
+
+; d)
+;; take-mcons-n :: Mcons(A...) Int -> ListOf[A]
+(define (take-mcons-n mut-pair n)
+  (if (zero? n)
+      '()
+      (append (list (mcar mut-pair)) (take-mcons-n (mcdr mut-pair) (- n 1)))))
+
+(test (take-mcons-n (mcons 1 (mcons 2 '())) 1) (list 1))
+(test (take-mcons-n (mcons 1 (mcons 2 (mcons 3 '()))) 2) (list 1 2))
+(test (take-mcons-n (mcons 5 (mcons 2 (mcons 3 (mcons 4 '())))) 3) (list 5 2 3))
 
 (define (interp a b) (void)) ; interprete
 (define box-extend-env (void)) ; debería ir aEnv
@@ -72,3 +100,26 @@
   (parTRO n #t))
 
 ; transformación
+
+(deftype Expr
+  (num n)
+  (add l r))
+
+;; <expr> ::= <num>
+;;          | (+ <num> <num>)
+(define (eval expr)
+  (match expr
+    [(num n) n]
+    [(add l r) (+ (eval l) (eval r))]))
+
+(test (eval (add (num 3) (num 5))) 8)
+
+;; TRO
+(define (eval-tr expr)
+  (define (eval-tail expr acc)
+    (match expr
+      [(num n) (+ acc n)]  ; Add the number to the accumulated result
+      [(add l r) (eval-tail l (eval-tail r acc))])) ; Recur on both sides
+  (eval-tail expr 0))
+
+(test (eval-tr (add (num 8) (num 5))) 13)
